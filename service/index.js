da2471f0555b2b547e04c768444987e68eb37d5a
Mayla105 <mailenamigob@gmail.com>
2021-03-01 16:49:00 -0300

    service completed - dependencies updated

---
+++ service/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors')
+const service = require('./service');
+
+const app = express();
+app.use(cors());
+
+//TODO req.query.q
+app.get('/api/items', (req, res) => {
+  service.getItemsList(req.query.q)
+      .then(items => res.json(items))
+      .catch(error => res.status(500).send(error));
+});
+
+
+app.get('/api/items/:id', (req, res) => {
+  service.getItemDetail(req.params.id)
+      .then(item => res.json(item))
+      .catch(error => res.status(error.status).send(error));
+});
+
+app.listen(5000, () => {
+  console.log('Server started in port 5000');
+});
---
+++ service/service.js
@@ -0,0 +1,97 @@
+const https = require('https');
+
+const apiURLS = {
+  search: 'https://api.mercadolibre.com/sites/MLA/search?q=',
+  itemDetail: 'https://api.mercadolibre.com/items/:id'
+}
+
+const author = {
+  name: 'Mailen',
+  lastname: 'Amigo Barzola'
+}
+
+exports.getItemsList = (query) => {
+  return search(query).then((response => {
+      const categories = response.filters.length && response.filters[0].values ?
+      response.filters[0].values[0].path_from_root.map((elem) => elem.name) : [];
+      const items = filterItems(response.results);
+      return {
+        author,
+        categories,
+        items
+      }
+  }))
+};
+
+function filterCategories(filters) {
+  return filters.length && filters[0].values ?
+  filters[0].values[0].path_from_root.map((elem) => elem.name) : [];
+}
+
+function filterItems(results) {
+  return results.map((item) => {
+    const [amount, decimals] = item.price.toString().split('.');
+    return {
+      id: item.id,
+      title: item.title,
+      price: {
+          currency: item.currency_id,
+          amount: parseInt(amount),
+          decimals: parseInt(decimals)
+      },
+      picture: item.thumbnail,
+      condition: item.condition,
+      free_shipping: item.shipping.free_shipping
+    }
+  });
+}
+
+exports.getItemDetail = (query) => {
+  return itemDetail(query).then(responses => {
+    const [item, description] = responses;
+    const [amount, decimals] = item.price.toString().split('.');
+    return {
+      author,
+      item: {
+        id: item.id,
+        title: item.title,
+        price: {
+            currency: item.currency_id,
+            amount: parseInt(amount),
+            decimals: parseInt(decimals)
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping,
+        sold_quantity: item.sold_quantity,
+        description: description.plain_text
+      }
+    }
+  });
+};
+
+//#Request 
+const search = (query) => {
+  console.log(`${apiURLS.search}${query}`)
+  return request(`${apiURLS.search}${query}`);
+};
+
+const itemDetail = (query) => {
+  return Promise.all(
+    [request(`${apiURLS.itemDetail}${query}`),
+    request(`${apiURLS.itemDetail}${query}/description`)]);
+};
+
+let request = (url) => {
+  return new Promise((resolve, reject) => {
+      let body = '';
+      https.get(url, (response) => {
+          response.on('data', (chunk) => {
+              body += chunk;
+          });
+          response.on('end', () => {
+              resolve(JSON.parse(body));
+          })
+      }).on('error', (e) => reject(e));
+  })
+};
