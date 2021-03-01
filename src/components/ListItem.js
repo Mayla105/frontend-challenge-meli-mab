fd6129b36c18e76fc96e219ba848478bcb140c41
Mayla105 <mailenamigob@gmail.com>
2021-03-01 20:37:37 -0300

    ListItem logic. Formatting utils

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as utils from '../utils';
 
+const ListItem = ({ info, categories }) => {
+  return (
+    <div>
+      <div id={info.id}>
+        <Link
+          to={{
+            pathname: `/items/${info.id}`,
+            itemInfo: info,
+            categories: categories,
+          }}
+        >
+          <img width='160' height='160' src={info.picture} alt={info.title} />
+        </Link>
+        <div>
+          <p>
+            {utils.priceFormatter(info.price)}
+            {info.price.decimals ? <span>,{info.price.decimals}</span> : null}
+          </p>
+          {info.free_shipping ? <small>Envio gratis</small> : null}
+          <Link
+            to={{
+              pathname: `/items/${info.id}`,
+              itemInfo: info,
+              categories: categories,
+            }}
+          >
+            <p>{info.title}</p>
+          </Link>
+        </div>
+        <div>
+          <p>{info.condition === 'new' ? 'Nuevo' : 'Usado'}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const ListItem = ({info, categories}) => {
-
-  return <div>each item</div>;
+ListItem.propTypes = {
+  info: PropTypes.object,
+  categories: PropTypes.array,
 };
 
 export default ListItem;
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+const addSeparators = price =>
+  price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+export const priceFormatter = price => {
+  return `${price.currency === 'ARS' ? '$ ' : 'U$S '}${addSeparators(
+    price.amount
+  )}`;
+};
