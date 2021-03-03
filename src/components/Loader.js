9a8185405ba034716d9eac7613e781acc73dff53
Mayla105 <mailenamigob@gmail.com>
2021-03-02 23:54:10 -0300

    loader styles

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Loader(){
-  return <div className='loader'></div>;
+  return <div className='container-fluid loader-container'>
+    <div className='row flex-grow-1 h-100 align-items-center justify-content-center'>
+      <div className='loader'></div>
+    </div>
+  </div>;
 }
